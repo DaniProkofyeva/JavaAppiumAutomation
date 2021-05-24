95cc884498bfe3306fce8f4b7e0316fae4e67975
DaniProkofyeva <daria.dm.leonova@gmail.com>
2021-05-25 01:27:56 +0300

    Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,16 +1,19 @@
 import org.junit.Test;
+import org.junit.Assert;
 
-public class MainClassTest extends MainClass
-{
+public class MainClassTest extends MainClass {
+
     @Test
-    public void testGetClassNumber()
-    {
-        int a = this.getClassNumber();
-
-        if (a > 45) {
-            System.out.println("Ok");
-        } else {
-            System.out.println("Bad");
+    public void testGetClassString() {
+        String phrase = this.getClassString();
+        if (phrase.contains("Hello")) {
+            return;
         }
+        if (phrase.contains("hello")) {
+            return;
+        } else {
+            Assert.fail("I have not Hello or hello");
+        }
+
     }
 }
